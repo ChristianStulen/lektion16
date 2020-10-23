2b7330a22aa0ab05f0e119bdf6e1341e310722f8
krizzlan <stulen.christian@gmail.com>
2020-10-23 11:57:13 +0200

    Added point class

---
+++ src/PointDemo.java
@@ -0,0 +1,17 @@
+
+
+public class PointDemo {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        p1.x = 4.5;
+        p1.y = 4.5;
+
+        p2.x = -3;
+        p2.y = 1.2;
+
+
+
+    }
+}
